21635252ed0b45f569f3e3b1bdfefee1d78a975c
Tejaswa <tejaswahinduja@gmail.com>
2025-11-27 18:42:36 +0530

    gemini working , need to try gptoss once again lets see

--- apps/backend/src/services/gpt.ts
+++ apps/backend/src/services/gpt.ts
@@ -1,10 +1,14 @@
 import { GoogleGenAI } from "@google/genai";
-
-const ai = new GoogleGenAI({});
+import dotenv from "dotenv"
+dotenv.config()
+const apikey=process.env.GEMINI_API_KEY;
+console.log(apikey)
+//@ts-ignore
+const ai = new GoogleGenAI({apikey});
 
 async function main() {
   const response = await ai.models.generateContent({
-    model: "gemini-3-pro-preview",
+    model: "gemini-2.5-flash",
     contents: "Explain how AI works in a few words",
   });
   console.log(response.text);
