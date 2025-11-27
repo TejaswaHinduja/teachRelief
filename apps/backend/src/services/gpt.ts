6c405402250db0c5e80602ac642c1e6b61def16c
Tejaswa <tejaswahinduja@gmail.com>
2025-11-27 18:02:01 +0530

    nahi chal raha bhai

--- apps/backend/src/services/gpt.ts
+++ apps/backend/src/services/gpt.ts
@@ -1,28 +1,12 @@
-import OpenAI from "openai";
-import dotenv from "dotenv"
-dotenv.config();
+import { GoogleGenAI } from "@google/genai";
 
-export async function compareAi(data:string){
-    
-const client = new OpenAI({
-	baseURL: "https://router.huggingface.co/v1",
-	apiKey: process.env.HF_TOKEN,
-});
+const ai = new GoogleGenAI({});
 
-const chatCompletion = await client.chat.completions.create({
-	model: "openai/gpt-oss-120b:nebius",
-    messages: [
-        {
-            role: "user",
-            content: "data",
-        },
-    ],
-});
-
-if(!chatCompletion){
-    return console.log(Error)
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-pro-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
 }
-//@ts-ignore
-console.log(chatCompletion.choices[0].message);
-}
-
+main();
