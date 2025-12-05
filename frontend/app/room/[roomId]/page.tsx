48c2be9319db0d2570d7b4809438cdae61bb2a0a
Tejaswa <tejaswahinduja@gmail.com>
2025-12-05 18:08:23 +0530

    submission flow working fine, next is add the grading logic

--- frontend/app/room/[roomId]/page.tsx
+++ frontend/app/room/[roomId]/page.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button"
 import {Label} from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import {useRouter,useParams} from "next/navigation";
 import { ImageKitAbortError, ImageKitInvalidRequestError, ImageKitServerError, ImageKitUploadNetworkError, upload, } from "@imagekit/next";
 import { Card,CardBody } from "@heroui/card";
@@ -11,18 +11,38 @@ import TeacherRoom from "./teacherRoom"
 import StudentRoom from "./studentRoom";
 
 export default function room(){
-  const [ocrText, setOcrText] = useState("");
   const [loading, setLoading] = useState(false);
-  const [uploading, setUploading] = useState(false);
   const [error, setError] = useState("");
   const [pdfUrl, setPdfUrl] = useState("");
   const [assignmentTitle, setAssignmentTitle] = useState(""); 
   const [creating, setCreating] = useState(false); 
   const [success, setSuccess] = useState(""); 
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const [userName, setUserName] = useState<string>("");
+  const router=useRouter()
+
+
+  useEffect(() => {
+      const role = localStorage.getItem("userRole");
+      const name = localStorage.getItem("userName");
+  
+      if (!role) {
+        router.push("/auth/login");
+        return;
+      }
+  
+      setUserRole(role);
+      setUserName(name || "User");
+    }, [router]);
 
 
   return (
     <div className="p-6 max-w-4xl mx-auto space-y-6">
-      <TeacherRoom/>
+      {userRole==="TEACHER"?(
+        <TeacherRoom/> ) :
+        (<StudentRoom/>
+      )
+      }
+      
     </div>
 )}
