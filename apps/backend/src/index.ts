0b2677f6d96f3867298dec734e44ce1f7c072f11
Tejaswa <tejaswahinduja@gmail.com>
2025-12-21 23:06:28 +0530

    add env later

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -12,7 +12,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://teachrelief/tejaswahinduja.me",
     credentials:true
 }))
 app.use("/api", router)
