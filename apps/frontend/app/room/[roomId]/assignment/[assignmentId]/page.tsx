d6abb9ffe9533614c0d513d704db8ca7b538fc8b
Tejaswa <tejaswahinduja@gmail.com>
2025-12-28 14:13:55 +0530

    view assignments working

--- apps/frontend/app/room/[roomId]/assignment/[assignmentId]/page.tsx
+++ apps/frontend/app/room/[roomId]/assignment/[assignmentId]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ImageKitAbortError, ImageKitInvalidRequestError, ImageKitServerError, ImageKitUploadNetworkError, upload, } from "@imagekit/next";
 import { useParams } from "next/navigation";
 import { Input } from "@/components/ui/input";
@@ -16,14 +16,15 @@ export default function Assignment() {
   const [pdfUrl, setPdfUrl] = useState("")
   const [ocrText, setOcrText] = useState("")
   const [sucess, setSuccess] = useState(false)
-  const [assignment,setAssignment]=useState<{
-  title: string;
-  pdfUrl: string;
-} | null>(null);
+  const [assignment, setAssignment] = useState<{
+    title: string;
+    pdfUrl: string;
+  } | null>(null);
 
   const [submitting, setSubmitting] = useState(false)
   const [loading, setLoading] = useState(false)    //const[assignmentId,setAssignmentId]=useState(false)
   const params = useParams();
+  const roomId=params.roomId as string;
   const assignmentId = params.assignmentId as string;
 
   const authenticator = async () => {
@@ -142,19 +143,19 @@ export default function Assignment() {
       throw new Error(errorData.message || "Failed to create assignment");
     }
   };
-  useEffect(()=>{
-   fetchAssignments()
-  },[assignmentId])
+  useEffect(() => {
+    fetchAssignments()
+  }, [assignmentId])
 
-   const fetchAssignments= async ()=>{
-      const response=await fetch(`${BACKEND_URL}/api/assignment/${assignmentId}`,{
-        method:"GET",
-        credentials:"include"
-      })
-      const data =await response.json()
-      setAssignment(data.assignmentDetails)
-
-    }
+  const fetchAssignments = async () => {
+    const response = await fetch(`${BACKEND_URL}/api/room/${roomId}/assignment/${assignmentId}`, {
+      method: "GET",
+      credentials: "include"
+    })
+    const data = await response.json()
+    setAssignment(data.assignmentDetails)
+    console.log(data.pdfUrl)
+  }
 
 
   return <div className="justify-center items-center py-4 space-y-4 space-x-2 min-h-screen">
@@ -163,7 +164,7 @@ export default function Assignment() {
         {assignmentId}
       </CardBody>
     </Card>
-  {/*<Card  className="border-gray-200 border-4 w-70" isPressable onPress={()=>{
+    {/*<Card  className="border-gray-200 border-4 w-70" isPressable onPress={()=>{
     setOpen(!open)
   }}>
   Click to view Assignment
@@ -173,9 +174,9 @@ export default function Assignment() {
     <iframe title="Assignment" src="https://ik.imagekit.io/tejaswahinduja/teachR_Gw3IGDXYj.pdf?updatedAt=1766822656304" className="w-200 h-500"></iframe>
   </div>
    } */}
-      <Card className="border-gray-300 border-2 w-45 translate-x-2" isPressable onPress={() => window.open(assignment?.pdfUrl, "_blank")}>
-            View Assignment 
-      </Card>
+    <Card className="border-gray-300 border-2 w-45 translate-x-2" isPressable onPress={() => window.open(assignment?.pdfUrl, "_blank")}>
+      View Assignment
+    </Card>
     <div className="py-4">
       <Input type="file" accept=".pdf" onChange={handleUpload}></Input>
     </div>
