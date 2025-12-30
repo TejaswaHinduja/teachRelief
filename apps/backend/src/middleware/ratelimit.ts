1b3b16b140ffc7d90301bd58479939431d76d137
Tejaswa <tejaswahinduja@gmail.com>
2025-12-30 15:49:01 +0530

    changed rate limit

--- apps/backend/src/middleware/ratelimit.ts
+++ apps/backend/src/middleware/ratelimit.ts
@@ -4,8 +4,9 @@ import { AuthRequest } from "./protect";
 
 export const ocrLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 4, // 4 OCR requests per hour per user
+  max: 4, // 4 OCR requests per hour per IP
   //@ts-ignore
-  keyGenerator:(req:AuthRequest)=>req.user.id,
+  //keyGenerator:(req:AuthRequest)=>req.user.id, //to make requests per user
+  
   message: { error: "OCR limit reached. Try again later." }
 });
